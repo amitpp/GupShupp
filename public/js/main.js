2785f1bdf187038d2614592fc4722b2063f27cd0
greenSyntax <ab.abhishek.ravi@gmail.com>
2018-04-04 01:14:47 +0530

    Intial Commit

---
+++ public/js/main.js
@@ -0,0 +1,110 @@
+
+var name = "";
+var socket_url = "http://localhost:3000";
+
+var socket = "";
+
+$(function(){
+
+  handleButtonEvents();
+
+});
+
+function initialize() {
+  $('#prompt_name_container').show();
+  $('#message_container').hide();
+}
+
+function clearText() {
+  $('#input_message').val('');
+}
+
+function showChatScreen() {
+  $('#prompt_name_container').hide();
+  $('#message_container').show();
+}
+
+function handleButtonEvents() {
+
+  $('button').click(function(){
+
+    if(this.id == 'btn_login'){
+      join()
+    }
+    else if (this.id == 'btn_send') {
+      send();
+    }
+    else if(this.id == 'btn_close') {
+      leave();
+    }
+
+  });
+}
+
+function createSocketConnection() {
+
+    socket = io(socket_url);
+
+    socket.on('message', function(data){
+
+      console.log("# sender : "+data.sender);
+      console.log("# message : "+data.text);
+
+      postToChat(data.sender, data.text);
+      clearText();
+    });
+
+    socket.on('online', function(data){
+
+      showChatScreen()
+      console.log("# ONLINE "+data.text);
+      postToChat(data.sender, data.text);
+    });
+
+    socket.on('disconnect', function(user){
+
+    });
+}
+
+function send() {
+
+  console.log($('#input_message').val().trim().length);
+  if($('#input_message').val().trim().length <= 0){
+    return;
+  }
+  else {
+    var message = $('#input_message').val();
+    var obj = {sender: name, text: message};
+    publish('message', obj);
+  }
+}
+
+function join() {
+  if($('#input_name').val().trim().length == 0) {
+    alert('Enter Your Name');
+  }
+  else {
+    name = $('#input_name').val().trim();
+
+    createSocketConnection()
+    publish('authentication', {username:name});
+  }
+
+}
+
+function publish(eventName, obj) {
+  socket.emit(eventName, obj);
+}
+
+function postToChat(sender, message) {
+  var completeText = '<span class="name">' + sender + '</span>'+"  "+message;
+  $('#messages').append($('<li>').html(completeText));
+
+}
+
+function leave() {
+  name = "";
+  socket = "";
+  console.log("Logout");
+  initialize();
+}
---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const path = require('path');
+var app = express();
+var bodyParser = require('body-parser');
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+const port = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname,'public')));
+
+activeUsers = [];
+
+// Esatblish Connection with Clients
+io.sockets.on('connection', function(socket){
+
+  // This method will be called, first
+  activeUsers.push(socket);
+  console.log("Connected Socket Clients : %s",activeUsers.length);
+
+  // Disconnect
+  socket.on('disconnect', function(){
+    activeUsers.splice(activeUsers.indexOf(socket), 1);
+    console.log("Disconnect: Remaining Clients %s", activeUsers.length);
+
+    io.sockets.emit('MESSAGE', {text: socket.username+" has left chat.", sender: 'Admin'});
+  });
+
+  // Subscriber : Getting Data
+  socket.on('message', function(data){
+
+    io.sockets.emit('message', {
+      text: data.text,
+      sender: data.sender
+    });
+  });
+
+  socket.on('authentication', function(user){
+
+    console.log("Got request of user : "+user.username);
+    //io.sockets.emit('message', {text: user.username+" is online.", sender: 'Bot'});
+    io.sockets.emit('online', {text: user.username+" is online.", sender: 'Bot'});
+  });
+
+  socket.on('DISCONNECT_USER', function(user){
+
+  });
+
+});
+
+http.listen(port, function(){
+  console.log("Server running at "+port);
+});
